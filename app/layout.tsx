c05921cc828035a5cefa73a37ac70500090db53c
George <gakpovwovwo@gmail.com>
2023-05-05 13:58:05 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Steelo World",
+  description: "NEXT13 With TypeScript",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
+    </html>
+  );
+}
